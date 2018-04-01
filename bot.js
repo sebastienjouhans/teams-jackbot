f94ad3f7f3f954cb6102ce42f7eee76c165c7d53
sebastien jouhans <sebastien.jouhans@gmail.com>
2018-04-01 11:35:42 +0100

    added who am i

--- bot.js
+++ bot.js
@@ -97,6 +97,16 @@ controller.hears(['templates_intent'], 'direct_message,direct_mention,mention', 
     }
 });
 
+controller.hears('who am i', 'direct_message, direct_mention', function(bot, message) {
+    bot.api.getUserById(message.channel, message.user, function(err, user) {
+        if (err) {
+          bot.reply(message,'Error loading user:' + err);
+        } else {
+          bot.reply(message,'You are ' + user.name + ' and your email is ' + user.email + ' and your user id is ' + user.id);
+        }
+    });
+});
+
 controller.on('direct_mention', function(bot, message) {
     bot.reply(message, 'You mentioned me and said, "' + message.text + '"');
 });
